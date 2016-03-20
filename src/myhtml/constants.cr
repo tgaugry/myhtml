4908cf11b328c55f69a5b3992a94979d3200552a
Konstantin Makarchev <kostya27@gmail.com>
2016-03-20 20:19:27 +0300

    change places of constatns

--- src/myhtml/constants.cr
+++ src/myhtml/constants.cr
@@ -1,46 +1,5 @@
 module Myhtml
   lib Lib
-    enum MyhtmlEncodingList
-      MyHTML_ENCODING_DEFAULT          = 0x00
-      MyHTML_ENCODING_AUTO             = 0x01 # future
-      MyHTML_ENCODING_CUSTOM           = 0x02 # future
-      MyHTML_ENCODING_UTF_8            = 0x00 # default encoding
-      MyHTML_ENCODING_UTF_16LE         = 0x04
-      MyHTML_ENCODING_UTF_16BE         = 0x05
-      MyHTML_ENCODING_X_USER_DEFINED   = 0x06
-      MyHTML_ENCODING_BIG5             = 0x07
-      MyHTML_ENCODING_EUC_KR           = 0x08
-      MyHTML_ENCODING_GB18030          = 0x09
-      MyHTML_ENCODING_IBM866           = 0x0a
-      MyHTML_ENCODING_ISO_8859_10      = 0x0b
-      MyHTML_ENCODING_ISO_8859_13      = 0x0c
-      MyHTML_ENCODING_ISO_8859_14      = 0x0d
-      MyHTML_ENCODING_ISO_8859_15      = 0x0e
-      MyHTML_ENCODING_ISO_8859_16      = 0x0f
-      MyHTML_ENCODING_ISO_8859_2       = 0x10
-      MyHTML_ENCODING_ISO_8859_3       = 0x11
-      MyHTML_ENCODING_ISO_8859_4       = 0x12
-      MyHTML_ENCODING_ISO_8859_5       = 0x13
-      MyHTML_ENCODING_ISO_8859_6       = 0x14
-      MyHTML_ENCODING_ISO_8859_7       = 0x15
-      MyHTML_ENCODING_ISO_8859_8       = 0x16
-      MyHTML_ENCODING_KOI8_R           = 0x17
-      MyHTML_ENCODING_KOI8_U           = 0x18
-      MyHTML_ENCODING_MACINTOSH        = 0x19
-      MyHTML_ENCODING_WINDOWS_1250     = 0x1a
-      MyHTML_ENCODING_WINDOWS_1251     = 0x1b
-      MyHTML_ENCODING_WINDOWS_1252     = 0x1c
-      MyHTML_ENCODING_WINDOWS_1253     = 0x1d
-      MyHTML_ENCODING_WINDOWS_1254     = 0x1e
-      MyHTML_ENCODING_WINDOWS_1255     = 0x1f
-      MyHTML_ENCODING_WINDOWS_1256     = 0x20
-      MyHTML_ENCODING_WINDOWS_1257     = 0x21
-      MyHTML_ENCODING_WINDOWS_1258     = 0x22
-      MyHTML_ENCODING_WINDOWS_874      = 0x23
-      MyHTML_ENCODING_X_MAC_CYRILLIC   = 0x24
-      MyHTML_ENCODING_LAST_ENTRY       = 0x25
-    end
-
     enum MyhtmlTags
       MyHTML_TAG__UNDEF              = 0x000
       MyHTML_TAG__TEXT               = 0x001
@@ -296,6 +255,47 @@ module Myhtml
       MyHTML_TAG__END_OF_FILE        = 0x0fb
       MyHTML_TAG_FIRST_ENTRY         = MyHTML_TAG__TEXT
       MyHTML_TAG_LAST_ENTRY          = 0x0fc
+    end
+
+    enum MyhtmlEncodingList
+      MyHTML_ENCODING_DEFAULT          = 0x00
+      MyHTML_ENCODING_AUTO             = 0x01 # future
+      MyHTML_ENCODING_CUSTOM           = 0x02 # future
+      MyHTML_ENCODING_UTF_8            = 0x00 # default encoding
+      MyHTML_ENCODING_UTF_16LE         = 0x04
+      MyHTML_ENCODING_UTF_16BE         = 0x05
+      MyHTML_ENCODING_X_USER_DEFINED   = 0x06
+      MyHTML_ENCODING_BIG5             = 0x07
+      MyHTML_ENCODING_EUC_KR           = 0x08
+      MyHTML_ENCODING_GB18030          = 0x09
+      MyHTML_ENCODING_IBM866           = 0x0a
+      MyHTML_ENCODING_ISO_8859_10      = 0x0b
+      MyHTML_ENCODING_ISO_8859_13      = 0x0c
+      MyHTML_ENCODING_ISO_8859_14      = 0x0d
+      MyHTML_ENCODING_ISO_8859_15      = 0x0e
+      MyHTML_ENCODING_ISO_8859_16      = 0x0f
+      MyHTML_ENCODING_ISO_8859_2       = 0x10
+      MyHTML_ENCODING_ISO_8859_3       = 0x11
+      MyHTML_ENCODING_ISO_8859_4       = 0x12
+      MyHTML_ENCODING_ISO_8859_5       = 0x13
+      MyHTML_ENCODING_ISO_8859_6       = 0x14
+      MyHTML_ENCODING_ISO_8859_7       = 0x15
+      MyHTML_ENCODING_ISO_8859_8       = 0x16
+      MyHTML_ENCODING_KOI8_R           = 0x17
+      MyHTML_ENCODING_KOI8_U           = 0x18
+      MyHTML_ENCODING_MACINTOSH        = 0x19
+      MyHTML_ENCODING_WINDOWS_1250     = 0x1a
+      MyHTML_ENCODING_WINDOWS_1251     = 0x1b
+      MyHTML_ENCODING_WINDOWS_1252     = 0x1c
+      MyHTML_ENCODING_WINDOWS_1253     = 0x1d
+      MyHTML_ENCODING_WINDOWS_1254     = 0x1e
+      MyHTML_ENCODING_WINDOWS_1255     = 0x1f
+      MyHTML_ENCODING_WINDOWS_1256     = 0x20
+      MyHTML_ENCODING_WINDOWS_1257     = 0x21
+      MyHTML_ENCODING_WINDOWS_1258     = 0x22
+      MyHTML_ENCODING_WINDOWS_874      = 0x23
+      MyHTML_ENCODING_X_MAC_CYRILLIC   = 0x24
+      MyHTML_ENCODING_LAST_ENTRY       = 0x25
     end
 
     enum MyhtmlStatus
