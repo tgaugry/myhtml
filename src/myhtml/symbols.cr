4d9d10d55033094a68062a60b304827bd3492e16
Konstantin Makarchev <kostya27@gmail.com>
2016-04-05 16:22:41 +0300

    fix

--- src/myhtml/symbols.cr
+++ src/myhtml/symbols.cr
@@ -4,9 +4,9 @@ module Myhtml
   macro gen_tag_id_by_symbol
     def self.tag_id_by_symbol(sym : Symbol)
       case sym
-      {% for name in Myhtml::Lib::MyhtmlTags.constants %}
+      {% for name in Lib::MyhtmlTags.constants %}
         when :{{ name.gsub(/MyHTML_TAG_/, "").downcase.id }}
-          Myhtml::Lib::MyhtmlTags::{{ name.id }}
+          Lib::MyhtmlTags::{{ name.id }}
       {% end %}
       else
         raise Error.new("Unknown tag #{sym.inspect}")
@@ -17,10 +17,10 @@ module Myhtml
   gen_tag_id_by_symbol
 
   macro gen_symbol_by_tag_id
-    def self.symbol_by_tag_id(tag_id : Myhtml::Lib::MyhtmlTags)
+    def self.symbol_by_tag_id(tag_id : Lib::MyhtmlTags)
       case tag_id
-      {% for name in Myhtml::Lib::MyhtmlTags.constants %}
-        when Myhtml::Lib::MyhtmlTags::{{ name.id }}
+      {% for name in Lib::MyhtmlTags.constants %}
+        when Lib::MyhtmlTags::{{ name.id }}
           :{{ name.gsub(/MyHTML_TAG_/, "").downcase.id }}
       {% end %}
       else
