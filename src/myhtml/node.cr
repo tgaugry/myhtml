0864bb29e69169f6ec97872ad5a9bd27eb8a1716
Konstantin Makarchev <kostya27@gmail.com>
2016-09-14 19:14:30 +0400

    refactor tag_id methods

--- src/myhtml/node.cr
+++ src/myhtml/node.cr
@@ -1,7 +1,11 @@
+require "./tag_id_utils"
+
 module Myhtml
   struct Node
     @node : Lib::MyhtmlTreeNodeT*
     @attributes : Hash(String, String)?
+
+    include TagIdUtils
 
     def self.from_raw(tree : Tree, raw_node : Lib::MyhtmlTreeNodeT*) : Node?
       unless raw_node.null?
@@ -34,10 +38,6 @@ module Myhtml
 
     def tag_id
       Lib.node_tag_id(@node)
-    end
-
-    def tag_sym
-      Myhtml.symbol_by_tag_id(tag_id)
     end
 
     def tag_name_slice
@@ -117,50 +117,6 @@ module Myhtml
 
     def right_iterator
       RightIterator.new(self)
-    end
-
-    def visible?
-      case tag_id
-      when Lib::MyhtmlTags::MyHTML_TAG_STYLE,
-           Lib::MyhtmlTags::MyHTML_TAG_COMMENT,
-           Lib::MyhtmlTags::MyHTML_TAG_SCRIPT,
-           Lib::MyhtmlTags::MyHTML_TAG_HEAD
-        false
-      else
-        true
-      end
-    end
-
-    def object?
-      case tag_id
-      when Lib::MyhtmlTags::MyHTML_TAG_APPLET,
-           Lib::MyhtmlTags::MyHTML_TAG_IFRAME,
-           Lib::MyhtmlTags::MyHTML_TAG_FRAME,
-           Lib::MyhtmlTags::MyHTML_TAG_FRAMESET,
-           Lib::MyhtmlTags::MyHTML_TAG_EMBED,
-           Lib::MyhtmlTags::MyHTML_TAG_OBJECT
-        true
-      else
-        false
-      end
-    end
-
-    {% for name in Lib::MyhtmlTags.constants %}
-      def is_tag_{{ name.gsub(/MyHTML_TAG_/, "").downcase.id }}?
-        tag_id == Lib::MyhtmlTags::{{ name.id }}
-      end
-    {% end %}
-
-    def is_text?
-      tag_id == Lib::MyhtmlTags::MyHTML_TAG__TEXT
-    end
-
-    def is_tag_noindex?
-      tag_id >= Lib::MyhtmlTags::MyHTML_TAG_LAST_ENTRY && tag_name_slice == "noindex".to_slice
-    end
-
-    def is_tag_nofollow?
-      tag_id >= Lib::MyhtmlTags::MyHTML_TAG_LAST_ENTRY && tag_name_slice == "nofollow".to_slice
     end
 
     def lastest_child
