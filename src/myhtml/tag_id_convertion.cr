7eccf47ea7794c834cbdec1652741d511d268856
Konstantin Makarchev <kostya27@gmail.com>
2016-11-20 21:53:31 +0300

    better generate macros

--- src/myhtml/tag_id_convertion.cr
+++ src/myhtml/tag_id_convertion.cr
@@ -1,35 +1,31 @@
 require "./constants"
 
 module Myhtml
-  macro gen_tag_id_by_symbol
-    def self.tag_id_by_symbol(sym : Symbol)
-      case sym
-      {% for name in Lib::MyhtmlTags.constants %}
-        when :{{ name.gsub(/MyHTML_TAG_/, "").downcase.id }}
-          Lib::MyhtmlTags::{{ name.id }}
-      {% end %}
-      else
-        raise Error.new("Unknown tag #{sym.inspect}")
-      end
+  def self.tag_id_by_symbol(sym : Symbol)
+    {% begin %}
+    case sym
+    {% for name in Lib::MyhtmlTags.constants %}
+      when :{{ name.gsub(/MyHTML_TAG_/, "").downcase.id }}
+        Lib::MyhtmlTags::{{ name.id }}
+    {% end %}
+    else
+      raise Error.new("Unknown tag #{sym.inspect}")
     end
+    {% end %}
   end
 
-  gen_tag_id_by_symbol
-
-  macro gen_symbol_by_tag_id
-    def self.symbol_by_tag_id(tag_id : Lib::MyhtmlTags)
-      case tag_id
-      {% for name in Lib::MyhtmlTags.constants %}
-        when Lib::MyhtmlTags::{{ name.id }}
-          :{{ name.gsub(/MyHTML_TAG_/, "").downcase.id }}
-      {% end %}
-      else
-        :unknown
-      end
+  def self.symbol_by_tag_id(tag_id : Lib::MyhtmlTags)
+    {% begin %}
+    case tag_id
+    {% for name in Lib::MyhtmlTags.constants %}
+      when Lib::MyhtmlTags::{{ name.id }}
+        :{{ name.gsub(/MyHTML_TAG_/, "").downcase.id }}
+    {% end %}
+    else
+      :unknown
     end
+    {% end %}
   end
-
-  gen_symbol_by_tag_id
 
   @@tags_string_sym_map : Hash(String, Symbol)?
 
