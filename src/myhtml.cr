c780f06d62abb2200406285b157564c2233833f6
Konstantin Makarchev <kostya27@gmail.com>
2016-11-23 18:35:00 +0300

    remove last parse method

--- src/myhtml.cr
+++ src/myhtml.cr
@@ -16,8 +16,7 @@ module Myhtml
   # TODO: rewrite to optimal, this is slow
   def self.decode_html_entities(str : String)
     return str if str.empty?
-    parser = Myhtml::Parser.new(tree_options: Myhtml::Lib::MyhtmlTreeParseFlags::MyHTML_TREE_PARSE_FLAGS_WITHOUT_DOCTYPE_IN_TREE)
-    parser.parse(str)
+    parser = Myhtml::Parser.new(str, tree_options: Myhtml::Lib::MyhtmlTreeParseFlags::MyHTML_TREE_PARSE_FLAGS_WITHOUT_DOCTYPE_IN_TREE)
     if child = parser.body!.child
       child.tag_text
     else
