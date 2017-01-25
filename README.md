85140c9078e159e3032fe97fa15a6b362bc5a95b
Konstantin Makarchev <kostya27@gmail.com>
2017-01-25 23:11:12 +0300

    better exampels

--- README.md
+++ README.md
@@ -29,35 +29,10 @@ And run `crystal deps`
 ## Usage
 
 ```crystal
-# Example: print all html tree
-
 require "myhtml"
-
-def walk(node, level = 0)
-  puts "#{" " * level}#{node.tag_name}#{node.attributes}(#{node.tag_text.strip})"
-  node.children.each { |child| walk(child, level + 1) }
-end
-
-str = if filename = ARGV[0]?
-        File.read(filename, "UTF-8", invalid: :skip)
-      else
-        "<html><Div><span class='test'>HTML</span></div></html>"
-      end
-
-parser = Myhtml::Parser.new(str)
-walk(parser.root!)
+page = "<html>...</html>"
+myhtml = Myhtml::Parser.new(page)
 ```
-
-Output:
-```
-html{}()
- head{}()
- body{}()
-  div{}()
-   span{"class" => "test"}()
-    -text{}(HTML)
-```
-
 
 ## More Examples
 
---
+++ examples/usage.cr
@@ -0,0 +1,55 @@
+# Example: basic usage
+
+require "../src/myhtml"
+
+page = "<html><div class=aaa>bla</div></html>"
+myhtml = Myhtml::Parser.new(page)
+
+# html node
+myhtml.root  # (.html) Myhtml::Node?
+myhtml.root! # (.html!) Myhtml::Node
+
+# body node
+myhtml.body  # Myhtml::Node?
+myhtml.body! # Myhtml::Node
+
+# head node
+myhtml.head  # Myhtml::Node?
+myhtml.head! # Myhtml::Node
+
+# iterator over all div nodes from root scope
+myhtml.nodes(Myhtml::Lib::MyhtmlTags::MyHTML_TAG_DIV) # Iterator(Myhtml::Node)
+myhtml.nodes(:div)                                    # Iterator(Myhtml::Node)
+myhtml.nodes("div")                                   # Iterator(Myhtml::Node)
+
+myhtml.nodes(:div).each do |node|
+  # node is Myhtml::Node
+  # methods:
+  pp node.tag_id                # => MyHTML_TAG_DIV
+  pp node.tag_sym               # => :div
+  pp node.tag_name              # => "div"
+  pp node.is_tag_div?           # => true
+  pp node.attribute_by("class") # => "aaa"
+  pp node.attributes            # => {"class" => "aaa"}
+
+  pp node.to_html    # => "<div class=\"aaa\">bla</div>"
+  pp node.inner_text # => "bla"
+  pp node.to_string  # inspect node: "<div {\"class\" => \"aaa\"}>"
+
+  # tree navigate methods (methods with !, returns not_nil! node):
+  node.child      # Myhtml::Node?, first child of node
+  node.next       # Myhtml::Node?, next node in the parent scope
+  node.parent     # Myhtml::Node?, parent node
+  node.prev       # Myhtml::Node?, previous node in the parent scope
+  node.left       # Myhtml::Node?, left node, in the html, from current
+  node.right      # Myhtml::Node?, right node, in the html, from current
+  node.flat_right # Myhtml::Node?, right node, in the html, from current, without node.children
+
+  # iterators:
+  node.children        # Iterator(Myhtml::Node), iterate over all direct node children
+  node.parents         # Iterator(Myhtml::Node), iterate over all node parents from current to root! node
+  node.scope           # Iterator(Myhtml::Node), iterate over all inner nodes (children and deeper)
+  node.right_iterator  # Iterator(Myhtml::Node), iterate from current node to right (to the end of document)
+  node.left_iterator   # Iterator(Myhtml::Node), iterate from current node to left (to the root! node)
+  node.scope.nodes(:a) # Iterator(Myhtml::Node), select :a nodes in scope of `node`
+end
