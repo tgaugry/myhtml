9a5996935ebb156acfa2a269ba06598143ba7685
Konstantin Makarchev <kostya27@gmail.com>
2017-02-16 15:27:14 +0300

    default join with space

--- src/myhtml/node.cr
+++ src/myhtml/node.cr
@@ -215,7 +215,7 @@ module Myhtml
     #   end
     # end
 
-    def inner_text(join_with : String | Char | Nil = nil, deep = true)
+    def inner_text(join_with : String | Char | Nil = ' ', deep = true)
       String.build do |buf|
         (deep ? scope : children).nodes(:_text).each_with_index do |node, i|
           buf << join_with if join_with && i != 0
