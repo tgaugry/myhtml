4046c4c052fe3c574f44b9d07296fe341f26ad78
Konstantin Makarchev <kostya27@gmail.com>
2017-02-26 02:00:21 +0300

    more encoding specs

--- examples/encoding.cr
+++ examples/encoding.cr
@@ -1,5 +1,5 @@
 require "../src/myhtml"
-page = File.read("./spec/fixtures/25.html")
+page = File.read("./spec/fixtures/25.htm")
 
 # by default page parsed as UTF-8
 myhtml = Myhtml::Parser.new(page)
@@ -8,6 +8,12 @@ p myhtml.nodes(:div).first.inner_text # => "\xC7\xE0\xE3\xF0\xF3\xE7\xEA\xE0...
 
 # set encoding directly
 myhtml = Myhtml::Parser.new(page, encoding: Myhtml::Lib::MyhtmlEncodingList::MyHTML_ENCODING_WINDOWS_1251)
+p myhtml.encoding # => MyHTML_ENCODING_WINDOWS_1251
+p myhtml.nodes(:div).first.inner_text # => "Загрузка. Пожалуйста, подождите..."
+
+# set encoding from header
+encoding = Myhtml.parse_charset("Content-Type: text/html; charset=Windows-1251")
+myhtml = Myhtml::Parser.new(page, encoding: encoding)
 p myhtml.encoding # => MyHTML_ENCODING_WINDOWS_1251
 p myhtml.nodes(:div).first.inner_text # => "Загрузка. Пожалуйста, подождите..."
 
--- spec/encoding_spec.cr
+++ spec/encoding_spec.cr
@@ -1,7 +1,8 @@
 require "./spec_helper"
 
-PAGE1 = File.read("./spec/fixtures/1.htm")
-PAGE2 = File.read("./spec/fixtures/2.htm")
+PAGE1  = File.read("./spec/fixtures/1.htm")
+PAGE2  = File.read("./spec/fixtures/2.htm")
+PAGE25 = File.read("./spec/fixtures/25.htm")
 
 describe Myhtml::Node do
   it "default" do
@@ -28,9 +29,43 @@ describe Myhtml::Node do
     parser.nodes(:div).first.inner_text.should eq "хаха"
   end
 
-  # it "detect by text" do
-  #   parser = Myhtml::Parser.new(PAGE2, detect_encoding: true)
-  #   parser.encoding.should eq Myhtml::Lib::MyhtmlEncodingList::MyHTML_ENCODING_WINDOWS_1251
-  #   parser.nodes(:div).first.inner_text.should eq "хаха"
-  # end
+  context "complex test" do
+    it "default" do
+      myhtml = Myhtml::Parser.new(PAGE25)
+      myhtml.encoding.should eq Myhtml::Lib::MyhtmlEncodingList::MyHTML_ENCODING_DEFAULT
+      myhtml.nodes(:div).first.inner_text.should contain "\xC7\xE0\xE3\xF0\xF3\xE7\xEA\xE0"
+    end
+
+    it "direct" do
+      myhtml = Myhtml::Parser.new(PAGE25, encoding: Myhtml::Lib::MyhtmlEncodingList::MyHTML_ENCODING_WINDOWS_1251)
+      myhtml.encoding.should eq Myhtml::Lib::MyhtmlEncodingList::MyHTML_ENCODING_WINDOWS_1251
+      myhtml.nodes(:div).first.inner_text.should eq "Загрузка. Пожалуйста, подождите..."
+    end
+
+    it "parse from header" do
+      encoding = Myhtml.parse_charset("Content-Type: text/html; charset=Windows-1251")
+      myhtml = Myhtml::Parser.new(PAGE25, encoding: encoding)
+      myhtml.encoding.should eq Myhtml::Lib::MyhtmlEncodingList::MyHTML_ENCODING_WINDOWS_1251
+      myhtml.nodes(:div).first.inner_text.should eq "Загрузка. Пожалуйста, подождите..."
+    end
+
+    it "parse from header" do
+      encoding = Myhtml.parse_charset("text/html; charset=Windows-1251")
+      myhtml = Myhtml::Parser.new(PAGE25, encoding: encoding)
+      myhtml.encoding.should eq Myhtml::Lib::MyhtmlEncodingList::MyHTML_ENCODING_WINDOWS_1251
+      myhtml.nodes(:div).first.inner_text.should eq "Загрузка. Пожалуйста, подождите..."
+    end
+
+    it "detect from meta" do
+      myhtml = Myhtml::Parser.new(PAGE25, detect_encoding_from_meta: true)
+      myhtml.encoding.should eq Myhtml::Lib::MyhtmlEncodingList::MyHTML_ENCODING_WINDOWS_1251
+      myhtml.nodes(:div).first.inner_text.should eq "Загрузка. Пожалуйста, подождите..."
+    end
+
+    it "detect from trigrams" do
+      myhtml = Myhtml::Parser.new(PAGE25, detect_encoding: true)
+      myhtml.encoding.should eq Myhtml::Lib::MyhtmlEncodingList::MyHTML_ENCODING_WINDOWS_1251
+      myhtml.nodes(:div).first.inner_text.should eq "Загрузка. Пожалуйста, подождите..."
+    end
+  end
 end
