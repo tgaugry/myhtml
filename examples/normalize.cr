c4040d681ebcc7d81c76a947414b61b453d90122
Konstantin Makarchev <kostya27@gmail.com>
2017-04-03 20:10:06 +0400

    fix

--- examples/normalize.cr
+++ examples/normalize.cr
@@ -1,5 +1,5 @@
 # Example: normalize input html
-#   (close not closed tags, replace entities, downcase attributes & tags names)
+#   (close not closed tags, replace entities, downcase attributes & tags names, remove comments)
 
 require "../src/myhtml"
 
@@ -17,6 +17,9 @@ str = if filename = ARGV[0]?
       end
 
 myhtml = Myhtml::Parser.new(str, tree_options: Myhtml::Lib::MyhtmlTreeParseFlags::MyHTML_TREE_PARSE_FLAGS_SKIP_WHITESPACE_TOKEN)
+
+myhtml.nodes(:_comment).each &.remove! # remove all comments
+
 puts myhtml.root!.to_html
 
 # Output:
