c4bb1d565c9c2df66fac685b61d1da677b004d6d
Konstantin Makarchev <kostya27@gmail.com>
2018-09-23 21:18:57 +0300

    fix

--- src/myhtml/node/css.cr
+++ src/myhtml/node/css.cr
@@ -1,10 +1,10 @@
 struct Myhtml::Node
   #
-  # Css selector with string rule in scope on the current_node
+  # Css selector with string rule in scope on the current node
   #   return Myhtml::Iterator::Collection
   #
   # Example:
-  #   node.css("div.red").each { |node| p node } # iterate over all divs with class `red` in the scope of node
+  #   some_node.css("div.red").each { |node| p node } # iterate over all divs with class `red` in the scope of node
   #
   def css(rule : String)
     filter = CssFilter.new(rule)
@@ -14,12 +14,12 @@ struct Myhtml::Node
   end
 
   #
-  # Css selector with filter in scope on the current_node
+  # Css selector with filter in scope on the current node
   #   return Myhtml::Iterator::Collection
   #
   # Example:
   #   filter = Myhtml::CssFilter.new("div.red")
-  #   node.css(filter).each { |node| p node } # iterator over all divs with class `red` in the scope of node
+  #   some_node.css(filter).each { |node| p node } # iterator over all divs with class `red` in the scope of node
   #
   def css(filter : CssFilter)
     filter.search_from(self)
@@ -30,7 +30,7 @@ struct Myhtml::Node
   #   this allows to free collection after block call and not waiting for GC
   #
   # Example:
-  #   node.css("div.red") { |collection| collection.each { |node| p node } }
+  #   some_node.css("div.red") { |collection| collection.each { |node| p node } }
   #
   def css(arg)
     collection = css(arg)
