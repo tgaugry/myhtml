eddc74ffdf2edd4efbb54b3b84bc8ca7cf611508
Konstantin Makarchev <kostya27@gmail.com>
2018-10-07 23:01:19 +0300

    make html_entities use tree

--- src/myhtml/utils/html_entities.cr
+++ src/myhtml/utils/html_entities.cr
@@ -1,9 +1,9 @@
 module Myhtml::Utils::HtmlEntities
   # !this object never freed!
-  HTML_ENTITIES_NODE = Myhtml::Parser.new("1", tree_options: Myhtml::Lib::MyhtmlTreeParseFlags::MyHTML_TREE_PARSE_FLAGS_WITHOUT_DOCTYPE_IN_TREE).body!.child!
+  HTML_ENTITIES_NODE2 = Myhtml::Tree.new.create_node(:_text)
 
-  def self.decode(str : String)
-    HTML_ENTITIES_NODE.tag_text_set(str, Lib::MyEncodingList::MyENCODING_DEFAULT)
-    HTML_ENTITIES_NODE.tag_text
+  def self.decode(str : String, encoding = nil)
+    HTML_ENTITIES_NODE2.tag_text_set(str, encoding || Lib::MyEncodingList::MyENCODING_DEFAULT)
+    HTML_ENTITIES_NODE2.tag_text
   end
 end
