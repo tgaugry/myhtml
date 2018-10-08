1d80be92807bddb163faed0efd6d41886e00984d
Konstantin Makarchev <kostya27@gmail.com>
2018-10-08 09:34:49 +0300

    use tag text direct in entities decode

--- src/myhtml/utils/html_entities.cr
+++ src/myhtml/utils/html_entities.cr
@@ -1,6 +1,6 @@
 module Myhtml::Utils::HtmlEntities
   # !this object never freed!
-  HTML_ENTITIES_NODE2 = Myhtml::Tree.new.create_node(:_text)
+  HTML_ENTITIES_NODE2 = Myhtml::Tree.new.create_node(Lib::MyhtmlTags::MyHTML_TAG__TEXT)
 
   def self.decode(str : String, encoding = nil)
     HTML_ENTITIES_NODE2.tag_text_set(str, encoding || Lib::MyEncodingList::MyENCODING_DEFAULT)
