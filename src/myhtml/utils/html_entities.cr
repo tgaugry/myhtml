6ef6f0beac25d8d2ac4432a55a764548542cdada
Konstantin Makarchev <kostya27@gmail.com>
2018-10-08 09:35:54 +0300

    remove node suffix

--- src/myhtml/utils/html_entities.cr
+++ src/myhtml/utils/html_entities.cr
@@ -1,9 +1,9 @@
 module Myhtml::Utils::HtmlEntities
   # !this object never freed!
-  HTML_ENTITIES_NODE2 = Myhtml::Tree.new.create_node(Lib::MyhtmlTags::MyHTML_TAG__TEXT)
+  HTML_ENTITIES_NODE = Myhtml::Tree.new.create_node(Lib::MyhtmlTags::MyHTML_TAG__TEXT)
 
   def self.decode(str : String, encoding = nil)
-    HTML_ENTITIES_NODE2.tag_text_set(str, encoding || Lib::MyEncodingList::MyENCODING_DEFAULT)
-    HTML_ENTITIES_NODE2.tag_text
+    HTML_ENTITIES_NODE.tag_text_set(str, encoding || Lib::MyEncodingList::MyENCODING_DEFAULT)
+    HTML_ENTITIES_NODE.tag_text
   end
 end
